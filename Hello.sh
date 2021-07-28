588d6f82b59bb1f3b4b40de1803897cd1dd632dd
john <john@Linhs-Macbook-Air.local>
2021-07-28 15:38:27 -0400

    Hello.sh CommandUsed.md Created

---
+++ Hello.sh
@@ -0,0 +1,8 @@
+#print top 3 movies of yours from the user
+echo 'Enter your top 3 movies'
+echo 'Movie 1 ' 
+read movie1
+#read -p 'Movie 1 ' movie1 
+read -p 'Movie 2 ' movie2 
+read -p 'Movie 3 ' movie3
+echo "Your top 3 movies are: $movie1, $movie2, $movie3"
